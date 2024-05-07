27c64418fdc114ae38c49dfdd8d681a68309eb1c
Erik Moqvist <erik.moqvist@gmail.com>
2024-05-07 20:05:13 +0200

    Continued.

--- Sources/Rist/Rist.swift
+++ Sources/Rist/Rist.swift
@@ -3,3 +3,22 @@ import librist
 public func ristVersion() -> String {    
     return String(cString: librist_version()!)
 }
+
+public class RistContext {
+    let context: OpaquePointer
+    
+    init(context: OpaquePointer) {
+        self.context = context
+    }
+}
+
+public func ristSenderCreate() -> RistContext? {
+    var context: OpaquePointer? = nil
+    let result = withUnsafeMutablePointer(to: &context) { contextPointer in
+        return rist_sender_create(contextPointer, RIST_PROFILE_SIMPLE, 0, nil)
+    }
+    guard result == 0, let context else {
+        return nil
+    }
+    return RistContext(context: context)
+}
