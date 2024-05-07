86aba6d3bc8f712c95a8eb7337ddde464e206060
Erik Moqvist <erik.moqvist@gmail.com>
2024-05-07 07:45:23 +0200

    Initial Commit

---
+++ Tests/RistTests/RistTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Rist
+
+final class RistTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
