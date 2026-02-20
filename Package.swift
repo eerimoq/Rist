2150487d322544e04666a44e759e808417b2ae9f
Erik Moqvist <erik.moqvist@gmail.com>
2026-02-20 10:15:34 +0100

    xcframework.

--- Package.swift
+++ Package.swift
@@ -17,6 +17,6 @@ let package = Package(
         .target(name: "Rist", dependencies: ["librist"]),
         .binaryTarget(name: "librist",
                       url: "https://github.com/eerimoq/Rist/releases/download/v0.3.0/librist.xcframework.zip",
-                      checksum: "8166f4a8f3c5840e650c20c8d77bdc79125b800071a457a252bde0610ca622bc")
+                      checksum: "3a7ad76b2b6b80bc05800884f371ff8947801e597555e361050cbdcf6a8e1814")
     ]
 )
